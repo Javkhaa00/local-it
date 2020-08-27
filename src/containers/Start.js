dd6ae8fc95b18891b714327909e93b33dac620ad
Javkhaa00 <liplicker0423@gmail.com>
2020-08-27 16:17:33 +0800

    feat: new things removed packages

---
+++ src/containers/Start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Start = () => {
+    return (
+        <div className="Start">
+            
+        </div>
+    )
+}
+
+export default Start;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Header from "../containers/header";
+import Start from '../containers/Start';
 
 export const Home = () => {
     return (
-        <Header />
+        <Start />
     )
 }
 
