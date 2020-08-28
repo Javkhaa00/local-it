0b25c2b69a136124aae0a5faee6eabf75de0469e
Javkhaa00 <liplicker0423@gmail.com>
2020-08-28 14:21:14 +0800

    feat: member section done

--- src/components/UsCard.js
+++ src/components/UsCard.js
@@ -3,13 +3,23 @@ import Data from '../json/us.json';
 
 export const UsCard = () => {
 
-    const returnCard = () => {
-        
-    };
+    const returnCard = (el) => {
+
+        if (!el) return null;
+
+        return (
+            <div className="whoweare--cards">
+                <img src={el.imgUrl} alt="profile" />
+                <span className="whoweare--cards--name">{el.name}</span>
+                <span className="whoweare--cards--position">{el.position}</span>
+                <span className="desc-text">{el.text}</span>
+            </div >
+        )
+    }
 
     return (
-        <div>
-            <returnCard />
+        <div className="whoweare--container">
+            {Data.map(returnCard)}
         </div>
     )
 };
--- src/containers/WhoWeAre.js
+++ src/containers/WhoWeAre.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { UsCard } from '../components/UsCard';
 
 const WhoWeAre = () => {
     return (
         <div className="whoweare">
-            <span className="title">Who we are?</span>
+            <span className="title" style={{paddingBottom: "40px"}}>Who we are?</span>
+            <UsCard />
         </div>
     )
 }
