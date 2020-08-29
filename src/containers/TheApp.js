1e6b3265d11b350a57985bd57ef1992a191b6879
Javkhaa00 <liplicker0423@gmail.com>
2020-08-29 14:17:55 +0800

    feat: addingg carousel package

--- src/containers/TheApp.js
+++ src/containers/TheApp.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/swiper.scss';
-
 
 const TheApp = () => {
     return (
         <div className="app">
             <span className="title">The App</span>
-            <Swiper
-                spaceBetween={0}
-                loop={true}
-                slidesPerView={2}
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-            </Swiper>
         </div>
     )
 }
