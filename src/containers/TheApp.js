1e4eaf1c93ee4fd2e34d8455e0eabbc141311e7d
Javkhaa00 <liplicker0423@gmail.com>
2020-08-31 17:54:15 +0800

    feat: fixing responsive

--- src/containers/TheApp.js
+++ src/containers/TheApp.js
@@ -5,20 +5,28 @@ const TheApp = () => {
         <div className="app">
             <span className="title" style={{ paddingBottom: "24px" }}>The App</span>
             <div className="app--carousel">
-                <div>
+                <div className="app--carousel--wrapper">
                     <img src="/assets/layer1.png" alt="pic1" />
+                    <span className="app--carousel--title">Features 1</span>
+                    <span className="app--carousel--text">
+                        Save time sifting through pages when you use LocalIT- all and only small businesses finally in only
+                    </span>
                 </div>
-                <div>
+                <div className="app--carousel--wrapper">
                     <img src="/assets/layer2.png" alt="pic1" />
+                    <span className="app--carousel--title">Features 2</span>
+                    <span className="app--carousel--text">
+                        Save time sifting through pages when you use LocalIT- all and only small businesses finally in only
+                    </span>
                 </div>
-                <div>
+                <div className="app--carousel--wrapper">
                     <img src="/assets/layer3.png" alt="pic1" />
+                    <span className="app--carousel--title">Features 3</span>
+                    <span className="app--carousel--text">
+                        Save time sifting through pages when you use LocalIT- all and only small businesses finally in only
+                    </span>
                 </div>
             </div>
-            <span className="app--carousel--title">Features 1</span>
-            <span className="app--carousel--text">
-                Save time sifting through pages when you use LocalIT- all and only small businesses finally in only
-            </span>
             <div className="app--circle1" />
             <div className="app--circle2" />
             <div className="app--circle3" />
